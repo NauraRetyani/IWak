6a5b8c65ca4334f007dc418b296a4a9b2f985fc8
naura <naura.retyani@gmail.com>
2020-02-06 22:37:50 +0700

    main

--- app/src/main/java/com/naura/idn/iwak/MainActivity.kt
+++ app/src/main/java/com/naura/idn/iwak/MainActivity.kt
@@ -2,11 +2,30 @@ package com.naura.idn.iwak
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.synnapps.carouselview.CarouselView
+import com.synnapps.carouselview.ImageListener
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val imageContentSlider = intArrayOf(
+    )
+
+    val imageContentListener: ImageListener = object : ImageListener {
+        override fun setImageForPosition(position: Int, imageView: ImageView?) {
+            imageView?.setImageResource(imageContentSlider[position])
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+        val carouselView = is_main as CarouselView
+        carouselView.setImageListener(imageContentListener)
+        carouselView.setPageCount(imageContentSlider.count())
+
     }
 }
