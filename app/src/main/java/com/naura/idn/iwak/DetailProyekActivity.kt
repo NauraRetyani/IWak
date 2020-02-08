75612d606885eb7719eca91f19625395c8e2a0ec
dhiyaulaulia <dhiyaul.aulia2003@gmail.com>
2020-02-08 18:08:06 +0700

    Detail Proyek

---
+++ app/src/main/java/com/naura/idn/iwak/DetailProyekActivity.kt
@@ -0,0 +1,12 @@
+package com.naura.idn.iwak
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailProyekActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_proyek)
+    }
+}
